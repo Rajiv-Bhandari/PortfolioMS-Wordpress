4967d1d4a6f72b1b7ab83b2830dd81ecf4766159
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-11-28 10:49:20 +0545

    contact us page templete

--- wp-content/themes/ngoportfolio/template-parts/contact.php
+++ wp-content/themes/ngoportfolio/template-parts/contact.php
@@ -6,6 +6,27 @@
 get_header();
 ?>
 
-<h1>this is CONTACT page </h1>
+<div class="contact-header">
+    <h1>Contact Us</h1>
+</div>
+
+<div class="contact-content">
+    <?php
+    $contact_query = new WP_Query(array(
+        'post_type' => 'page',
+        'pagename' => 'contact', // Replace 'contact-us' with your actual page slug
+    ));
+
+    if ($contact_query->have_posts()) {
+        while ($contact_query->have_posts()) {
+            $contact_query->the_post();
+            the_content();
+        }
+        wp_reset_postdata();
+    } else {
+        echo 'Contact page content not found.';
+    }
+    ?>
+</div>
 
 <?php get_footer(); ?>
